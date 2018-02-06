6ac5c2bc2a2e89f5444759f5b7ea4a022012eff9
ashleygrudzinski <apg3@illinois.edu>
2018-02-06 12:47:56 -0600

    Worked on WebScraper

--- src/WebScrapper.java
+++ src/WebScrapper.java
@@ -1,5 +1,41 @@
+import java.io.IOException;
+import java.net.URL;
+import java.util.Scanner;
+
 public class WebScrapper {
+    /**
+     * Retrieve contents from a URL and return them as a string.
+     *
+     * @param url url to retrieve contents from
+     * @return the contents from the url as a string, or an empty string on error
+     */
+    public static String urlToString(final String url) {
+        Scanner urlScanner;
+        try {
+            urlScanner = new Scanner(new URL(url).openStream(), "UTF-8");
+        } catch (IOException e) {
+            return "";
+        }
+        String contents = urlScanner.useDelimiter("\\A").next();
+        urlScanner.close();
+        return contents;
+    }
+
+    public static int numCount(String url) {
+        String a = urlToString(url);
+
+        int count = 0;
+        for (int i = 0; i < a.length() - 6; i++) {
+            if (a.substring(i, i + 6).toLowerCase().equals("prince")) {
+                count++;
+            }
+
+        }
+        return count;
+    }
+
     public static void main(String[] unused){
         System.out.println("Run");
+        System.out.println(numCount("http://erdani.com/tdpl/hamlet.txt"));
     }
 }
